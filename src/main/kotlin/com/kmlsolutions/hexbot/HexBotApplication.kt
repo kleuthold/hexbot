2a9f372efd413748b72badff78d11aba10d6ed01
Kevin Leuthold <kevin.leuthold@barometerit.com>
2019-10-27 21:08:14 -0500

    play randomly

--- src/main/kotlin/com/kmlsolutions/hexbot/HexBotApplication.kt
+++ src/main/kotlin/com/kmlsolutions/hexbot/HexBotApplication.kt
@@ -16,14 +16,17 @@ class HexBotApplication {
         val screenSize = Toolkit.getDefaultToolkit().screenSize
         val robot = Robot()
 
-        (1 until 100).forEach { _ ->
+        (1 until 15).forEach { _ ->
             val image = robot.createScreenCapture(Rectangle(0, 0, screenSize.width, screenSize.height))
             val count = ScreenDetector().countPixelsInColorRange(image, 0, 50, 0, 50, 0, 50)
             if(count > 200000) log.info { "found $count dark pixels, i might be looking at the superhexio screen" }
             Thread.sleep(100)
         }
 
-        log.info("done")
+        while(true) {
+            robot.mouseMove((0 until screenSize.width).random(), (0 until screenSize.height).random())
+            Thread.sleep(100)
+        }
     }
 }
 
